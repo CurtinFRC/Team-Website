9feb2c546898e1e94a21c26db46f8e3b7b34922d
Isaac Turner <spacey_sooty@outlook.com>
2024-01-08 19:59:38 +0800

    Lol fix :+1:  (#99)
    * fixed
    
    * fix lol
    
    * add break

--- src/pages/robots.tsx
+++ src/pages/robots.tsx
@@ -11,9 +11,10 @@ export default function Home() {
       <div className="page">
         <div className='hero-no-padding'>
           <div className="column">
-            <Image src="src/components/5663_2019.jpg" alt="Our 2019 robot KillSwitch" width={600} height={336}/>
+            <Image src="/5663_2019.jpg" alt="Our 2019 robot KillSwitch" width={600} height={336}/>
             <p>Our 2019 robot KillSwitch: 
-                  Designed to be capable of scoring cargo quickly with a small hatch panel &apos;flooper&apos; this robot has been described as a @curtin.edu.aucargo beast@curtin.edu.au
+                  Designed to be capable of scoring cargo quickly with a small hatch panel &apos;flooper&apos; this robot has been described as a cargo beast
+		<br/>
                   Competed at:
                   South Pacific Regional - Lost in Semi Finals
             </p>
