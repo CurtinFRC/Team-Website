1349678d07775f5b966b0711c975d83e2febc7ab
Harry C <34387466+Hazzer890@users.noreply.github.com>
2024-01-08 19:35:39 +0800

    Update Home page and start work on robots page (#98)

--- src/pages/robots.tsx
+++ src/pages/robots.tsx
@@ -11,7 +11,12 @@ export default function Home() {
       <div className="page">
         <div className='hero-no-padding'>
           <div className="column">
-            {/* <Image src="/robot2023.jpg" alt="Our 2023 robot Whiplash" width={600} height={336}/> */}
+            <Image src="src/components/5663_2019.jpg" alt="Our 2019 robot KillSwitch" width={600} height={336}/>
+            <p>Our 2019 robot KillSwitch: 
+                  Designed to be capable of scoring cargo quickly with a small hatch panel &apos;flooper&apos; this robot has been described as a @curtin.edu.aucargo beast@curtin.edu.au
+                  Competed at:
+                  South Pacific Regional - Lost in Semi Finals
+            </p>
           </div>
         </div>
         <div className='heros'> </div>
