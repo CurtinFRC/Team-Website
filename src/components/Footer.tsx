27b867ce694a2223b940842906d0d1af3c00ae7c
Isaac Turner <spacey_sooty@outlook.com>
2024-04-04 16:49:32 +0800

    fix build

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,14 +14,13 @@ function Footer() {
               <Link href="/sponsors">Defence Australa</Link><br/>
               <Link href="/sponsors">Altronics</Link><br/>
             </div>
-          
+
         </div>
         <div id='footer-license'>
           <Logo id='cool-logo'/><br/>
-          {/* <Image id='cool-logo' src='public/' alt='MIT License' height={100} width={20}*/}
-          <p id="license-text">MIT License</p> 
+          <p id="license-text">MIT License</p>
 
-          <p>&copy; 2024 Can't Control Curtin FRC Team </p>
+          <p>&copy; 2024 Can&apos;t Control Curtin FRC Team </p>
         </div>
         <div id='footer-socials'>
           <p>Socials!</p>
@@ -30,10 +29,7 @@ function Footer() {
               <Link href="https://www.instagram.com/waroboticsplayoffs/">Instagram</Link><br/>
               <Link href="https://www.youtube.com/@CurtinFRC4788CantControl">Youtube</Link><br/>
             </div>
-          
         </div>
-        
-				
       </div>
     );
 	// }
