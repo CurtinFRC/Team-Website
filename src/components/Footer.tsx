36021fd5af955ecf0f835c53d67028cf3db50efb
Jade Turner <spacey-sooty@proton.me>
2024-06-17 21:53:12 +0800

    format

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import Link from 'next/link';
 
 function Footer() {
-    return (
-        <div className='footer'>
-            <p>&copy; 2024 Can&apos;t Control Curtin FRC Team </p>
-            <Link href='https://www.facebook.com/CurtinFrc/'>Facebook</Link>
-            <Link href='https://www.instagram.com/waroboticsplayoffs/'>
-                Instagram
-            </Link>
-            <Link href='https://www.youtube.com/@CurtinFRC4788CantControl'>
-                Youtube
-            </Link>
-        </div>
-    );
+  return (
+    <div className='footer'>
+      <p>&copy; 2024 Can&apos;t Control Curtin FRC Team </p>
+      <Link href='https://www.facebook.com/CurtinFrc/'>Facebook</Link>
+      <Link href='https://www.instagram.com/waroboticsplayoffs/'>
+        Instagram
+      </Link>
+      <Link href='https://www.youtube.com/@CurtinFRC4788CantControl'>
+        Youtube
+      </Link>
+    </div>
+  );
 }
 
 export default Footer;
--- src/pages/warp.tsx
+++ src/pages/warp.tsx
@@ -5,78 +5,72 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
-    var w = 1397;
-    var h = 1468;
-    return (
-        <>
-            <Head>
-                <title>WARP | 4788</title>
-                <meta
-                    name='description'
-                    content="WARP Curtin FRC's offseason competition"
-                />
-            </Head>
-            <div className='page'>
-                <div className='hero-no-padding'>
-                    <div className='column'>
-                        <Image
-                            className='logo'
-                            id='logo-filter'
-                            src='/warp-logo.png'
-                            alt='warp logo'
-                            width={w}
-                            height={h}
-                        />
-                        <Marquee speed={25} gradient={false} style={{ opacity: 0.8 }}>
-                            <h1>Warp</h1>
-                        </Marquee>
-                        <Arrows className='arrows' />
-                    </div>
-                </div>
-                <div className='heros'>
-                    <p>
-                        Curtin University, Murdoch University and the WA Department of
-                        Primary Industries and Regional Development are proud to bring you
-                        the WA Robotics Playoffs (WARP). WARP includes an off-season robot
-                        competition as part of the global FRC program. Teams are well
-                        underway with building their robots ready to compete at Curtin
-                        Stadium. Spectators are welcome at this event. Come along and see
-                        robot alliances competing for points. Find out how to get a team
-                        together for the 2024 competition.
-                    </p>
-                    <div id='parent'>
-                        <h2>
-                            <Link
-                                href='https://www.instagram.com/waroboticsplayoffs/'
-                            >
-                                Instagram
-                            </Link>
-                        </h2>
-                    </div>
-                    <div id='parent'>
-                        <h2>
-                            <Link
-                                href='https://www.facebook.com/people/WA-Robotics-Playoffs/100094239134377/'
-                            >
-                                Facebook
-                            </Link>
-                        </h2>
-                        <div style={{ marginLeft: '20%' }} className='popup' id='popup2'>
-                            <p>Bang</p>
-                        </div>
-                    </div>
-                    <div id='parent'>
-                        <h2>
-                            <Link
-                                href='https://www.youtube.com/@waroboticsplayoffs'
-                            >
-                                Youtube
-                            </Link>
-                        </h2>
-                    </div>
-                    <p style={{ marginBottom: '5%' }}></p>
-                </div>
+  var w = 1397;
+  var h = 1468;
+  return (
+    <>
+      <Head>
+        <title>WARP | 4788</title>
+        <meta
+          name='description'
+          content="WARP Curtin FRC's offseason competition"
+        />
+      </Head>
+      <div className='page'>
+        <div className='hero-no-padding'>
+          <div className='column'>
+            <Image
+              className='logo'
+              id='logo-filter'
+              src='/warp-logo.png'
+              alt='warp logo'
+              width={w}
+              height={h}
+            />
+            <Marquee speed={25} gradient={false} style={{ opacity: 0.8 }}>
+              <h1>Warp</h1>
+            </Marquee>
+            <Arrows className='arrows' />
+          </div>
+        </div>
+        <div className='heros'>
+          <p>
+            Curtin University, Murdoch University and the WA Department of
+            Primary Industries and Regional Development are proud to bring you
+            the WA Robotics Playoffs (WARP). WARP includes an off-season robot
+            competition as part of the global FRC program. Teams are well
+            underway with building their robots ready to compete at Curtin
+            Stadium. Spectators are welcome at this event. Come along and see
+            robot alliances competing for points. Find out how to get a team
+            together for the 2024 competition.
+          </p>
+          <div id='parent'>
+            <h2>
+              <Link href='https://www.instagram.com/waroboticsplayoffs/'>
+                Instagram
+              </Link>
+            </h2>
+          </div>
+          <div id='parent'>
+            <h2>
+              <Link href='https://www.facebook.com/people/WA-Robotics-Playoffs/100094239134377/'>
+                Facebook
+              </Link>
+            </h2>
+            <div style={{ marginLeft: '20%' }} className='popup' id='popup2'>
+              <p>Bang</p>
             </div>
-        </>
-    );
+          </div>
+          <div id='parent'>
+            <h2>
+              <Link href='https://www.youtube.com/@waroboticsplayoffs'>
+                Youtube
+              </Link>
+            </h2>
+          </div>
+          <p style={{ marginBottom: '5%' }}></p>
+        </div>
+      </div>
+    </>
+  );
 }
