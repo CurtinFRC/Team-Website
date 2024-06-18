6dd64ecac9c1b767ffa8e73f52dd947b5f157384
Jade Turner <spacey-sooty@proton.me>
2024-06-18 18:14:51 +0800

    move to react icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import Link from 'next/link';
+import { FaFacebook, FaGithub, FaInstagram, FaYoutube } from 'react-icons/fa';
 
 function Footer() {
   return (
     <div className='footer'>
-      <p>&copy; 2024 Can&apos;t Control Curtin FRC Team </p>
-      <Link href='https://www.facebook.com/CurtinFrc/'>Facebook</Link>
-      <Link href='https://www.instagram.com/waroboticsplayoffs/'>
-        Instagram
-      </Link>
-      <Link href='https://www.youtube.com/@CurtinFRC4788CantControl'>
-        Youtube
-      </Link>
+      <p style={{ justifyContent: 'left' }}>
+        &copy; 2024 Can&apos;t Control Curtin FRC Team{' '}
+      </p>
+      <FaGithub size={20} style={{ padding: 0, justifyContent: 'right' }}>
+        <Link href='https://github.com/CurtinFRC' />
+      </FaGithub>
+      <FaFacebook size={20}>
+        <Link href='https://www.facebook.com/CurtinFrc/' />
+      </FaFacebook>
+      <FaInstagram size={20}>
+        <Link href='https://www.instagram.com/waroboticsplayoffs/' />
+      </FaInstagram>
+      <FaYoutube size={20}>
+        <Link href='https://www.youtube.com/@CurtinFRC4788CantControl' />
+      </FaYoutube>
     </div>
   );
 }
