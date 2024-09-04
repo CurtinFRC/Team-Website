6340ec93eb464ca76b6e4a92376a981255098b70
Jade Turner <spacey-sooty@proton.me>
2024-09-04 16:50:51 +0800

    fix: migrate off class components
    Doesn't do navbar because I have a skill issue.

--- src/components/Sponsor.tsx
+++ src/components/Sponsor.tsx
@@ -3,24 +3,20 @@ import Image from 'next/image';
 
 type Props = { title: string; body: string; img: string; flipped: boolean };
 
-export default class Sponsor extends React.Component<Props, {}> {
-  constructor(props: Props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className='sponsor'>
-        <div className={'row${this.props.flipped ? reverse : 0}'}>
-          <div className='text'>
-            <h2>{this.props.title}</h2>
-            {/* <p>{this.props.body}</p> */}
-          </div>
-          <div className='image'>
-            <Image src={this.props.img} alt='' fill={true} />
-          </div>
+const Sponsor = (props: Props) => {
+  return (
+    <div className='sponsor'>
+      <div className={'row${props.flipped ? reverse : 0}'}>
+        <div className='text'>
+          <h2>{props.title}</h2>
+          {/* <p>{this.props.body}</p> */}
+        </div>
+        <div className='image'>
+          <Image src={props.img} alt='' fill={true} />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Sponsor;
